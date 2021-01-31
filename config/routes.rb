10d65771081d9f229b1b8e9e8f44d20b75b5d3d7
shisoshiso <66207602+shisoshiso@users.noreply.github.com>
2021-01-31 10:47:02 +0900

    rootパスのルーティング編集
    ・commentsコントローラーも編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  resources :comments
-  resources :recipes
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'recipes#index'
+  resources :recipes do
+    resources :comments, only: :create
+  end
 end
