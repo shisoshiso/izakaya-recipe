d9bdb2ee083f454d114c041154b7ccbd1f7271b1
shisoshiso <k.soshi1219@gmail.com>
2021-02-09 10:07:56 +0900

    userモデル　バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,9 @@ class User < ApplicationRecord
   has_many :recipes
   has_many :comments
 
-  validates :nickname, presence: true, length: {maximum: 30} #uniqueはマイグレーションファイルに記述
+  with_options presence: true do
+    validates :nickname, length: {maximum: 30} #uniqueはマイグレーションファイルに記述
+    validates :password, format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+\z/, message: 'は半角英数字混合での入力が必要です' }
+  end
+  
 end
