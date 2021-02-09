2bac193cece62d7ed7603786abd63eafde0f53b6
shisoshiso <k.soshi1219@gmail.com>
2021-02-09 11:53:11 +0900

    userモデル単体テストコード作成
    bundle exec rspec spec/models/user_spec.rb

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,62 @@
+require 'rails_helper'
+describe User do
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
+  describe 'ユーザー新規登録' do
+    context '新規登録がうまくいくとき' do
+      it 'nicknameとpassword、password_confirmationが存在すれば登録できる' do
+        expect(@user).to be_valid
+      end
+    end
+
+    context '新規登録がうまくいかないとき' do
+      it 'nicknameが空だと登録できない' do
+        @user.nickname = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include "Nickname can't be blank"
+      end
+      it '重複したnicknameが存在する場合登録できない' do
+        @user.save
+        another_user = FactoryBot.build(:user)
+        another_user.nickname = @user.nickname
+        another_user.valid?
+        expect(another_user.errors.full_messages).to include("Nickname そのニックネームはすでに使用されています")
+      end
+      it 'passwordが空では登録できない' do
+        @user.password = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include "Password can't be blank"
+      end
+      it 'passwordが5文字以下であれば登録できない' do
+        @user.password = 'aaa11'
+        @user.valid?
+        expect(@user.errors.full_messages).to include 'Password is too short (minimum is 6 characters)'
+      end
+      it 'passwordは英語のみでは登録できない' do
+        @user.password = 'aaaaaa'
+        @user.password_confirmation = 'aaaaaa'
+        @user.valid?
+        expect(@user.errors.full_messages).to include 'Password は半角英数字混合での入力が必要です'
+      end
+      it 'passwordは数字のみでは登録できない' do
+        @user.password = '111111'
+        @user.password_confirmation = '111111'
+        @user.valid?
+        expect(@user.errors.full_messages).to include 'Password は半角英数字混合での入力が必要です'
+      end
+      it 'passwordは全角では登録できない' do
+        @user.password = 'ａａａ１１１'
+        @user.password_confirmation = 'ａａａ１１１'
+        @user.valid?
+        expect(@user.errors.full_messages).to include 'Password は半角英数字混合での入力が必要です'
+      end
+      it 'passwordが存在してもpassword_confirmationが空では登録できない' do
+        @user.password_confirmation = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include "Password confirmation doesn't match Password"
+      end
+    end
+  end
+end
