205c6fc364ae9a31457f0d323649031a0e3746c9
shisoshiso <k.soshi1219@gmail.com>
2021-02-12 23:42:37 +0900

    recipeモデル　バリデーション設定

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,4 +2,12 @@ class Recipe < ApplicationRecord
   belongs_to :user
   has_one_attached :image
   
+  with_options presence: true do
+    validates :name, length: {maximum: 30}
+    validates :material
+    validates :alcohol_id
+    validates :category_id
+    validates :genre_id
+  end
+
 end
