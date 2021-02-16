cc7689cfc6ce913889d562cb4b0d9361a29669b0
shisoshiso <k.soshi1219@gmail.com>
2021-02-16 11:03:36 +0900

    rubocop修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,11 @@
 class CommentsController < ApplicationController
-
   def create
     comment = Comment.create(comment_params)
     redirect_to "/recipes/#{comment.recipe.id}"
   end
 
   private
+
   def comment_params
     params.require(:comment).permit(:text).merge(user_id: current_user.id, recipe_id: params[:recipe_id])
   end
