b64c4b45ce8e4a923913264f55d1628dc3ca93d4
shisoshiso <k.soshi1219@gmail.com>
2021-02-16 12:08:54 +0900

    ルーティング・フォームの設定

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,9 @@ Rails.application.routes.draw do
   resources :recipes
   resources :recipes do
     resources :comments, only: :create
+    collection do
+      get 'search'
+    end
   end
+  resources :users, only: :show
 end
