e1f48d1230394e4bfaa549bb9d75ca31863e6270
shisoshiso <k.soshi1219@gmail.com>
2021-02-16 10:43:45 +0900

    recipes、commentsコントローラーの編集

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,19 +1,9 @@
 class CommentsController < ApplicationController
-  before_action :set_comment, only: %i[show edit update destroy]
 
   # POST /comments or /comments.json
   def create
-    @comment = Comment.new(comment_params)
-
-    respond_to do |format|
-      if @comment.save
-        format.html { redirect_to @comment, notice: 'Comment was successfully created.' }
-        format.json { render :show, status: :created, location: @comment }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @comment.errors, status: :unprocessable_entity }
-      end
-    end
+    comment = Comment.create(comment_params)
+    redirect_to "/recipes/#{comment.recipe.id}"
   end
 
   # # PATCH/PUT /comments/1 or /comments/1.json
@@ -39,14 +29,7 @@ class CommentsController < ApplicationController
   # end
 
   private
-
-  # Use callbacks to share common setup or constraints between actions.
-  def set_comment
-    @comment = Comment.find(params[:id])
-  end
-
-  # Only allow a list of trusted parameters through.
   def comment_params
-    params.require(:comment).permit(:text, :user_id, :recipe_id)
+    params.require(:comment).permit(:text).merge(user_id: current_user.id, recipe_id: params[:recipe_id])
   end
 end
