7fbf26b141c2628f6763ec3f483c9af48b6b6dbe
shisoshiso <k.soshi1219@gmail.com>
2021-02-17 22:02:30 +0900

    user#indexページ削除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,10 @@
 class UsersController < ApplicationController
-
-  def index
-    @users = User.all
-    @followers = current_user.followers
-    @followings = current_user.followings
-  end
-
   def show
     @user = User.find(params[:id])
     @nickname = @user.nickname
     @recipes = @user.recipes.order("created_at DESC")
     @favorited = @user.favorited.order("created_at DESC")
+    @followers = current_user.followers
+    @followings = current_user.followings
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'recipes#index'
   
-  resources :users, only: [:index, :show]
+  resources :users, only: :show
   resources :relationships, only: [:create, :destroy]
 
   resources :recipes do
