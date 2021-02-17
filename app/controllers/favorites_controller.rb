3dd707950e29ce75024c618e229b85e6bc7b6bea
shisoshiso <k.soshi1219@gmail.com>
2021-02-17 13:04:49 +0900

    favoritesコントローラー作成
    rails routes すると、
    post_favorites
    DELETE /posts/:post_id/favorites　favorites#destroy
    POST /posts/:post_id/favorites　　favorites#create
    と表示されます。
    :post_idの部分をcontrollerで取得

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,2 +1,13 @@
 class FavoritesController < ApplicationController
+  def create
+    @favorite = current_user.favorites.create(recipe_id: params[:recipe_id])
+    redirect_back(fallback_location: root_path)
+  end
+
+  def destroy
+    @recipe = Post.find(params[:recipe_id])
+    @favorite = current_user.favorites.find_by(recipe_id: @recipe.id)
+    @favorite.destroy
+    redirect_back(fallback_location: root_path)
+  end
 end
