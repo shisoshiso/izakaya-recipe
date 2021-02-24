222b42ba1beb75074ecbbcf3116a7603048fbd40
shisoshiso <k.soshi1219@gmail.com>
2021-02-24 11:02:47 +0900

    ゲストログインを削除不可に
    現状使わないが。

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,6 +3,14 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
+  before_action :check_guest, only: :destroy
+
+  def check_guest
+    if resource.nickname == 'guest'
+      redirect_to root_path, alert: 'ゲストユーザーは削除できません。'
+    end
+  end
+
 
   # GET /resource/sign_up
   # def new
