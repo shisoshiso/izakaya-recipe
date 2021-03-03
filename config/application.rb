6db965a9e21bb2d4aa118f912ece34a8a3dfef80
shisoshiso <k.soshi1219@gmail.com>
2021-03-03 11:26:15 +0900

    出品時エラーメッセージの修正

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,7 @@ module IzakayaRecipe
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
     config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.yml').to_s]
   end
 end
 
