3ae4ad31ed31d5007caf0c0620eb94e2e1143a7c
shisoshiso <k.soshi1219@gmail.com>
2021-03-08 22:07:43 +0900

    パンくずリストの設定、表示

---
+++ config/breadcrumbs.rb
@@ -0,0 +1,34 @@
+crumb :root do
+  link "居酒屋レシピ トップ", root_path
+end
+
+crumb :recipe_new do
+  link "レシピ新規投稿", new_recipe_path
+  parent :root
+end
+
+crumb :recipe_search do
+    link "「#{params["keyword"]}」のレシピ検索結果", search_recipes_path
+  parent :root
+end
+
+crumb :about do
+  link "居酒屋レシピとは", page_path('about')
+  parent :root
+end
+
+crumb :recipe_show do
+  link "レシピ詳細表示", recipe_path
+  parent :root
+end
+
+crumb :recipe_edit do
+  link "レシピ編集", edit_recipe_path
+  parent :recipe_show
+end
+
+# If you want to split your breadcrumbs configuration over multiple files, you
+# can create a folder named `config/breadcrumbs` and put your configuration
+# files there. All *.rb files (e.g. `frontend.rb` or `products.rb`) in that
+# folder are loaded and reloaded automatically when you change them, just like
+# this file (`config/breadcrumbs.rb`).
