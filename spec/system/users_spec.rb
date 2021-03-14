cef2eb77a2b775c9016ded7813b8a7138d6fa685
shisoshiso <k.soshi1219@gmail.com>
2021-03-14 21:07:43 +0900

    ユーザー管理機能結合テスト

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -1,11 +1,35 @@
 require 'rails_helper'
 
-RSpec.describe 'ユーザー新規登録', type: :system do
-  before do
-    @user = FactoryBot.build(:user)
+RSpec.describe 'ユーザー管理機能', type: :system do
+  context 'ユーザー管理機能動作確認' do
+    it 'ログインしていない状態で新規投稿ページにアクセスした場合、サインインページに移動する' do
+      # 新規投稿ページに遷移する
+      visit new_recipe_path
+      # ログインしていない場合、サインインページに遷移していることを確認する
+      expect(current_path).to eq new_user_session_path
+    end
+
+    it 'ログインしていない状態でトップページから詳細検索を実行した場合、サインインページに移動する' do
+      # トップページに遷移する
+      visit root_path
+      # 詳細検索を行う（詳細検索ボタンをクリックする）
+      click_on('詳細検索')
+      # ログインしていない場合、サインインページに遷移していることを確認する
+      expect(current_path).to eq new_user_session_path
+    end
+
+    it 'ログインしていない状態でユーザー詳細ページにアクセスした場合、サインインページに移動する' do
+      # トップページに遷移する
+      visit new_recipe_path
+      # ログインしていない場合、サインインページに遷移していることを確認する
+      expect(current_path).to eq new_user_session_path
+    end
   end
 
-  context 'ユーザー新規登録ができるとき' do
+  context 'ユーザー新規登録' do
+    before do
+      @user = FactoryBot.build(:user)
+    end
     it '正しい情報を入力すればユーザーは新規登録ができてトップページに移動する' do
       # トップページに移動する
       visit root_path
@@ -15,7 +39,7 @@ RSpec.describe 'ユーザー新規登録', type: :system do
       visit new_user_registration_path
       # ユーザー情報を入力する fill_inの第一引数は、id名
       # ラベルのforの値とinputのidの値が一致していることで、ブラウザ上に表示されている'ユーザー名'をフォームと認識して取得
-      # ラベルが無いときや、forが無いときはidの値を入れることも可
+      # ラベルが無い、forが無いときはidの値を入れる
       fill_in 'nickname', with: @user.nickname
       fill_in 'password', with: @user.password
       fill_in 'password-confirmation', with: @user.password_confirmation
@@ -29,58 +53,37 @@ RSpec.describe 'ユーザー新規登録', type: :system do
       expect(page).to have_no_content('ゲストログイン')
     end
   end
+
+  context 'ユーザーログイン' do
+    # 予め、ユーザーをDBに保存する
+    before do
+      @user = FactoryBot.create(:user)
+    end
+    it 'ログインに成功し、トップページに遷移する' do
+      # サインインページへ移動する
+      visit new_user_session_path
+      # すでに保存されているユーザーのnicknameとpasswordを入力する
+      fill_in 'nickname', with: @user.nickname
+      fill_in 'password', with: @user.password
+      # ログインボタンをクリックする
+      click_on ('ログイン')
+      # トップページに遷移していることを確認する
+      expect(current_path).to eq root_path
+    end
+
+    it 'ログインに失敗し、再びサインインページに戻ってくる' do
+      # トップページに遷移する
+      visit new_user_session_path
+      # 誤ったユーザー情報を入力する
+      fill_in 'nickname',with: ""
+      fill_in 'password',with: ""
+      # ログインボタンをクリックする
+      click_on('ログイン')
+      # サインインページに戻ってきていることを確認する
+      expect(current_path).to eq new_user_session_path
+    end
+  end
 end
 
 
-RSpec.describe "ユーザーログイン機能", type: :system do
-  it 'ログインしていない状態で新規投稿ページにアクセスした場合、サインインページに移動する' do
-    # トップページに遷移する
-    visit new_recipe_path
-    # ログインしていない場合、サインインページに遷移していることを確認する
-    expect(current_path).to eq new_user_session_path
-  end
 
-  it 'ログインに成功し、トップページに遷移する' do
-    # 予め、ユーザーをDBに保存する
-    @user = FactoryBot.create(:user)
-
-    # サインインページへ移動する
-    visit root_path
-
-    # ログインしていない場合、サインインページに遷移していることを確認する
-    expect(current_path).to eq new_user_session_path
-
-    # すでに保存されているユーザーのemailとpasswordを入力する
-    fill_in 'user_email', with: @user.email
-    fill_in 'user_password', with: @user.password
-
-    # ログインボタンをクリックする
-    click_on ('Log in')
-
-    # トップページに遷移していることを確認する
-    expect(current_path).to eq root_path
-
-  end
-
-  it 'ログインに失敗し、再びサインインページに戻ってくる' do
-    # 予め、ユーザーをDBに保存する
-    @user = FactoryBot.create(:user)
-
-    # トップページに遷移する
-    visit root_path
-
-    # ログインしていない場合、サインインページに遷移していることを確認する
-    expect(current_path).to eq new_user_session_path
-
-    # 誤ったユーザー情報を入力する
-    fill_in 'user_email',with: "aa@ss"
-    fill_in 'user_password',with: "123456"
-
-    # ログインボタンをクリックする
-    click_on('Log in')
-
-    # サインインページに戻ってきていることを確認する
-    expect(current_path).to eq new_user_session_path
-
-  end
-end
